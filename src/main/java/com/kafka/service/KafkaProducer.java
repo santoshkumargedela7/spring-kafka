8eedfa1ad89e5e7135d57ae9159c417a0101edda
santoshkumargedela7 <santoshkumargedela7@gmail.com>
2023-02-20 09:53:44 +0530

    kafka subject

---
+++ src/main/java/com/kafka/service/KafkaProducer.java
@@ -0,0 +1,23 @@
+package com.kafka.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.kafka.utils.AppConstants;
+
+@Service
+public class KafkaProducer {
+		
+	private static final Logger logger = LoggerFactory.getLogger(KafkaProducer.class);
+	
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+	
+	public void sendMessage(String message) {
+		logger.info(String.format("Message sent ->%s",message));
+		kafkaTemplate.send(AppConstants.TOPIC_NAME,message);
+	}
+}
---
+++ src/main/java/com/kafka/utils/AppConstants.java
@@ -0,0 +1,7 @@
+package com.kafka.utils;
+
+public class AppConstants {
+	
+	public static final String TOPIC_NAME = "javaguides";
+    public static final String GROUP_ID = "group_id";
+}
